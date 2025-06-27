1d0b0ffd4af1ef41bd31633721d2521057bab6b0
JoJoWitness <jordano470@gmail.com>
2025-06-27 00:49:56 -0400

    Created all the csv files

---
+++ Server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"Server/config"
+	"Server/models"
+	"Server/routes"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	models.DropDB(config.PsqlDB)
+	models.InitDB(config.PsqlDB)
+	models.LoadSampleData(config.PsqlDB)
+
+	router := mux.NewRouter()
+	routes.Init(router)
+
+	srv := &http.Server{
+		Addr:         "localhost:8080",
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      router,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		fmt.Println(err)
+	}
+}
